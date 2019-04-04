d5a42332753a5d8bb14204de118e5d9e31889060
jasonevans <jasonevans@10.1.154.250>
2019-04-04 23:30:48 +0100

    add cart item, this is an object product with get methods for each variable stored in the object.

---
+++ IStore/src/java/mypkg/CartItem.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypkg;
+
+/**
+ *
+ * @author jasonevans
+ */
+public class CartItem {
+    private String category;
+    private int itemID;
+    private String itemName;
+    private String itemDsc;
+    private float itemPrice;
+    private int qtyOrdered;
+    
+    
+    public CartItem(String category, int itemID, String itemName, String itemDsc, float itemPrice, int qtyOrdered){
+        this.category = category;
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemDsc = itemDsc;
+        this.itemPrice = itemPrice;
+        this.qtyOrdered = qtyOrdered;
+    }
+    public int getItemID(){
+        return itemID;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public String getItemName(){
+        return itemName;
+    }
+    public String getItemDsc(){
+        return itemDsc;
+    }
+    public float getItemPrice(){
+        return itemPrice;
+    }
+    public int getQtyOrdered(){
+        return qtyOrdered;
+    }
+    public void setQtyOrdered(int qtyOrdered){
+        this.qtyOrdered = qtyOrdered;
+    }
+    
+}
